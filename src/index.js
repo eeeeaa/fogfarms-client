c8f79ba9e320256969f7f890a525009d246fe21a
eeeeaa <tigerinwmak@gmail.com>
2020-02-17 13:14:45 +0700

    -create general file structure

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import ThemeSwitcher from './ThemeSwitcher';
+import ThemeSwitcher from './MainLayout/ThemeSwitcher';
 import * as serviceWorker from './serviceWorker';
 var element = React.createElement('h1',{className: 'Greeting'}, 'Sup,Bitches!')
 ReactDOM.render(<ThemeSwitcher />, document.getElementById('root'));
