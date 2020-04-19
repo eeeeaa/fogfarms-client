2687ed83285fd12dca997864b87b1ef232f126ac
unknown <panutatr@hotmail.com>
2020-04-20 01:58:20 +0700

    fig the useEffect bugs

--- src/MainLayout/contexts/ModuleContext.js
+++ src/MainLayout/contexts/ModuleContext.js
@@ -7,18 +7,17 @@ export const ModuleContext = createContext();
 const ModuleContextProvider = (props) => {
 	const [modules, setModules] = useState([]);
 
-	useEffect(async ()=> {
+	useEffect(()=> {
 		const url = 'https://salty-oasis-24147.herokuapp.com';
-		const result = await app.get(url + '/modulegroup_management');
-		const data = result.data;
-		// console.log(result.data.modulegroup1.on_auto);
-		const modulesJson = Object.keys(result.data).map((key,i) => {
-			return {...data[key]};
-		})
-		// console.log(modulesJson);
-		// console.log("module_group_id : " + modulesJson[0].module_group_id);
-		setModules(modulesJson);
+		app.get(url + '/modulegroup_management').then((res) => {
+			const data = res.data;
+			const modulesJson = Object.keys(data).map((key,i) => {
+				return {...data[key]};
+			})
+			setModules(modulesJson);
+		});
 	}, [])
+
 	return <ModuleContext.Provider value={{modules}}>{props.children}</ModuleContext.Provider>;
 };
 
