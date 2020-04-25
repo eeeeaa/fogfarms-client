6e38190bfc8518dc640823d5dc01f250f9a430dd
eeeeaa <tigerinwmak@gmail.com>
2020-04-25 13:58:30 +0700

    -add plant and user management pages(WIP)

--- src/MainLayout/Manage_Plant.js
+++ src/MainLayout/Manage_Plant.js
@@ -1,15 +1,103 @@
 import React from 'react';
+import axios from 'axios';
+import app from '../axiosConfig';
 import '../css_sheet/global_theme.css';
-import { Container, Row,Col } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup,ListGroupItem, Button,Tooltip,OverlayTrigger,Modal } from 'react-bootstrap';
 
-function Manage_plant(){
-    return(
-        <Container fluid className="Management_container">
+const serverName = "https://salty-oasis-24147.herokuapp.com"
+
+class Manage_plant extends React.Component{
+    constructor(props) {
+      super(props);
+      this.state = {plants: [],current_plant:''};
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = this.handleClick.bind(this);
+    }
+    
+      componentDidMount() {
+        app.get(serverName + "/plant_management")
+          .then(res => {
+            const plt_data = res.data;
+            this.setState({plants:plt_data});
+            console.log("state", this.state.plants)
+          }).catch(error => console.log(error))
+      }
+      handleClick(item) {
+        const plant_name = item;
+        this.setState({current_plant:plant_name});
+        console.log("current plant",this.state.current_plant);
+      }
+      MyVerticallyCenteredModal(props) {
+        return (
+          <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                    Plant
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {
+                props.children
+              }
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        );
+      }
+      renderTooltip(props) {
+        return (
+          <Tooltip id="button-tooltip" {...props}>
+            Click to view plant information
+          </Tooltip>
+        );
+      }
+
+    render(){
+        
+        return(
+            
+            <Container fluid className='center-screen'>
+            <Row className='Panels'>
+                <Col align='center'>
+                    <Card style={{ width: '40rem' }}>
+                        <Card.Body>
+                            <Card.Title>Plants</Card.Title>
+                        </Card.Body>
+                        <ListGroup variant="dark">
+                            {/*Users*/}
+                            { 
+                            Object.keys(this.state.plants)
+                            .map(item => 
+                                <ListGroupItem action onClick={() => this.handleClick(item)}>
+                                  {item}
+                                  </ListGroupItem>, this)
+                            }
+                        </ListGroup>
+                        
+                    </Card>
+                </Col>
+                
+            </Row>
             <Row>
-                <Col>Manage Plant</Col>
+                <Col align='center'>
+                    <Button variant='dark'>
+                        Create new Plant  
+                    </Button>
+                    <Button variant='dark'>
+                        Delete Plant  
+                    </Button>
+                </Col>
             </Row>
         </Container>
-    )
+        );
+    }
 }
 
 export default Manage_plant;
--- src/MainLayout/Manage_User.js
+++ src/MainLayout/Manage_User.js
@@ -1,24 +1,121 @@
 import React from 'react';
+import axios from 'axios';
+import app from '../axiosConfig';
 import '../css_sheet/global_theme.css';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup,ListGroupItem, Button,Tooltip,OverlayTrigger,Modal } from 'react-bootstrap';
 
-function Manage_user(){
-    return(
-        <Container fluid className='center-screen'>
+const serverName = "https://salty-oasis-24147.herokuapp.com"
+
+class Manage_user extends React.Component{
+    constructor(props) {
+      super(props);
+      this.state = {users: [],current_user:''};
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = this.handleClick.bind(this);
+    }
+    
+      componentDidMount() {
+        app.get(serverName + "/user_management")
+          .then(res => {
+            const usr_data = res.data;
+            this.setState({users:usr_data});
+            console.log("state", this.state.users)
+          }).catch(error => console.log(error))
+      }
+      handleClick(item) {
+        const id = item;
+        this.setState({current_user:id});
+        console.log("current user",this.state.current_user);
+      }
+      MyVerticallyCenteredModal(props) {
+        return (
+          <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Register user
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {
+                props.children
+              }
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        );
+      }
+      renderTooltip(props) {
+        return (
+          <Tooltip id="button-tooltip" {...props}>
+            Click to change module group
+          </Tooltip>
+        );
+      }
+
+    render(){
+        
+        return(
+            
+            <Container fluid className='center-screen'>
             <Row className='Panels'>
-                <Col align='center'>User Panel</Col>
-                <Col align='center'>ModuleGroup permission Panel</Col>
-            </Row>
-            <Row className='Buttons'>
                 <Col align='center'>
-                    <Button>
-                        Enter
+                    <Card style={{ width: '40rem' }}>
+                        <Card.Body>
+                            <Card.Title>Users</Card.Title>
+                        </Card.Body>
+                        <ListGroup variant="dark">
+                            {/*Users*/}
+                            { 
+                            Object.keys(this.state.users)
+                            .map(item => 
+                                <ListGroupItem action onClick={() => this.handleClick(item)}>
+                                  {item}
+                                  </ListGroupItem>, this)
+                            }
+                        </ListGroup>
+                        
+                    </Card>
+                </Col>
+                <Col align='center'>
+                    <Card style={{ width: '40rem' }}>
+                        <Card.Body>
+                            <Card.Title>Module groups</Card.Title>
+                        </Card.Body>
+                        <ListGroup variant="dark">
+                          {/*module group permission levels*/}
+                          {
+                            this.state.current_user !== '' ?
+                            Object.keys(this.state.users[this.state.current_user])
+                            .map(item =>
+                                <ListGroupItem>
+                                    {item}
+                                </ListGroupItem>, this
+                            )
+                            :
+                            <div></div>          
+                          }
+                            
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col align='center'>
+                    <Button variant='dark'>
+                        Register new user  
                     </Button>
                 </Col>
-                <Col align='center'>Trash</Col>
             </Row>
         </Container>
-    );
+        );
+    }
 }
 
 export default Manage_user;
