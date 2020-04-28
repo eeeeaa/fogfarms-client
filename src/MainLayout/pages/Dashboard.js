6c05ba5ab123232342c292023931564a6159eae7
Rosbas <panutatr@hotmail.com>
2020-04-28 14:33:35 +0700

    add bootstrap back to the branch

--- src/MainLayout/pages/Dashboard.js
+++ src/MainLayout/pages/Dashboard.js
@@ -1,38 +1,42 @@
-import React from "react";
-import "../../css_sheet/global_theme.css";
-import ModuleSelect from "../components/ModuleGroup/ModuleSelect";
-import ModuleControllerHeader from "../components/ModuleController/ModuleControllerHeader";
-import ModuleDataContextProvider from "../contexts/ModuleDataContext";
-import { Container, Row, Col } from "react-bootstrap";
-import ModuleControllerTabs from "../components/ModuleController/ModuleControllerTabs";
-import "../../css_sheet/dashboard.css";
-import "../../css_sheet/notFromCode.css";
-import ModuleDataDetail from "../components/ModuleData/ModuleDataDetail";
-import ModuleContextProvider from "../contexts/ModuleContext";
+import React from 'react';
+import '../../css_sheet/global_theme.css';
+import ModuleSelect from '../components/ModuleGroup/ModuleSelect';
+import ModuleControllerHeader from '../components/ModuleController/ModuleControllerHeader';
+import ModuleDataContextProvider from '../contexts/ModuleDataContext';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import ModuleControllerTabs from '../components/ModuleController/ModuleControllerTabs';
+import '../../css_sheet/dashboard.css';
+import '../../css_sheet/notFromCode.css';
+import ModuleDataDetail from '../components/ModuleData/ModuleDataDetail';
+import ModuleContextProvider from '../contexts/ModuleContext';
 
 const Dashboard = () => {
-  return (
-    <div className="dashBoard">
-      <Container>
-        <Row>
-          <ModuleDataContextProvider>
-            <ModuleContextProvider>
-              <Col>
-                <ModuleSelect />
-              </Col>
-              <Col xs={6}>
-                <ModuleDataDetail />
-              </Col>
-              <Col>
-                <ModuleControllerHeader />
-                <ModuleControllerTabs />
-              </Col>
-            </ModuleContextProvider>
-          </ModuleDataContextProvider>
-        </Row>
-      </Container>
-    </div>
-  );
+	return (
+		<div className="dashBoard">
+			<Container>
+				<Row>
+					<ModuleDataContextProvider>
+						<ModuleContextProvider>
+							<Col>
+								<ModuleSelect />
+							</Col>
+							<Col xs={6}>
+								<ModuleDataDetail />
+							</Col>
+							<Col>
+								<Card>
+									<Card.Body>
+										<ModuleControllerHeader />
+										<ModuleControllerTabs />
+									</Card.Body>
+								</Card>
+							</Col>
+						</ModuleContextProvider>
+					</ModuleDataContextProvider>
+				</Row>
+			</Container>
+		</div>
+	);
 };
 
 export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css_sheet/index.css";
+import './css_sheet/bootstrap.min.css';
 import Main from "./MainLayout/Main";
 import * as serviceWorker from "./serviceWorker";
 
